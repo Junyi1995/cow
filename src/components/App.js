78cb419f6d01ebd74e21be4cc71c6cd69328d8fa
Junyi1995 <jh2635@cornell.edu>
2019-05-04 15:29:38 -0400

    edit metric

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,6 @@
 import React, { Component } from 'react';
-import { CowTemperature } from "./CowTemperature";
-import { BarnTemperature } from "./BarnTemperature";
-import { OutsideTemperature } from "./OutsideTemperature";
-import { Route } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
+import  Main  from './Main';
 import  Metric  from "./metric";
 //import { Header } from './Main'
 import '../styles/App.css';
@@ -11,23 +9,12 @@ class App extends Component {
   //state = {isLoggedIn: ture}
   render(){
     return (
-      <div className="wrapper">
-        <div className="header">
-          <img src="../../assets/images/cow.png"
-               alt=""
-          />
-          <div className="date-range">06/05/2018-07/05/2018</div>
-        </div>
-        <div className="split-bar">Overview</div>
-        {/*data cube*/}
-        <div className="data-wrapper">
-          <CowTemperature/>
-          <BarnTemperature/>
-          <OutsideTemperature/>
-        </div>
-        <div className="route">
+
+      <div className="app">
+        <Switch>
           <Route path="/loaderio-780e704a35ef881a24df1fd93fe75962/" component = {Metric}/>
-        </div>
+          <Route path="/dashboard" component = { Main } />
+        </Switch>
       </div>
     );
   }
--- src/components/metric.js
+++ src/components/metric.js
@@ -3,7 +3,9 @@ import React, { Component } from 'react';
 export default class Metrics extends Component {
   render(){
     return(
-      <p className="metric">loaderio-780e704a35ef881a24df1fd93fe75962</p>
+      <div className="main">
+        <p className="metric">loaderio-780e704a35ef881a24df1fd93fe75962</p>
+      </div>
     );
   }
 }
