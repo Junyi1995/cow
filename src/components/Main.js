9e7b1db90867ece7fc97aacf8a1555519475e4d4
Junyi1995 <jh2635@cornell.edu>
2019-05-04 15:35:09 -0400

    delete server.js

---
+++ src/components/Main.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import { CowTemperature } from "./CowTemperature";
+import { BarnTemperature } from "./BarnTemperature";
+import { OutsideTemperature } from "./OutsideTemperature";
+import '../styles/App.css';
+
+class Main extends Component {
+  //state = {isLoggedIn: ture}
+  render(){
+    return (
+
+      <div className="wrapper">
+        <div className="header">
+          <img src="../../assets/images/cow.png"
+               alt=""
+          />
+          <div className="date-range">06/05/2018-07/05/2018</div>
+        </div>
+        <div className="split-bar">Overview</div>
+        {/*data cube*/}
+        <div className="data-wrapper">
+          <CowTemperature/>
+          <BarnTemperature/>
+          <OutsideTemperature/>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Main;
